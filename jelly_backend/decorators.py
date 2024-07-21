f57ac651b202205ad979f3eb7cb7c28ddb462d90
BrayanQuirozUrrutia <brayanquirozurrutia@gmail.com>
2024-07-21 17:23:16 -0400

    Cambio en docker

--- jelly_backend/decorators.py
+++ jelly_backend/decorators.py
@@ -7,25 +7,22 @@ from graphql import GraphQLError
 def validate_token(func):
     @wraps(func)
     def wrapper(self, info, *args, **kwargs):
-        # Obtener la solicitud del contexto
         request = info.context.get('request')
         if request is None:
             raise GraphQLError('No se pudo obtener la solicitud.')
 
-        # Leer las cookies de la solicitud
-        cookies = request.COOKIES
+        # Imprimir el contexto para depuración
+        print("Contexto de la solicitud:", info.context)
 
-        # Obtener el token de acceso desde las cookies
+        cookies = request.COOKIES
         access_token = cookies.get('access_token')
         if not access_token:
             raise GraphQLError('Debes iniciar sesión')
 
         try:
-            # Validar el token de acceso
             access_token_obj = AccessToken(access_token)
         except TokenError:
             raise GraphQLError('Token inválido')
 
         return func(self, info, *args, **kwargs)
-
     return wrapper
