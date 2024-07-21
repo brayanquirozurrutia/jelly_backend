b732fade8341998a0b1330082f4713799e0eac22
BrayanQuirozUrrutia <brayanquirozurrutia@gmail.com>
2024-07-21 16:53:19 -0400

    Cambio en docker

--- jelly_backend/decorators.py
+++ jelly_backend/decorators.py
@@ -7,15 +7,14 @@ from graphql import GraphQLError
 def validate_token(func):
     @wraps(func)
     def wrapper(self, info, *args, **kwargs):
-        authorization_header = info.context.headers.get('Authorization')
+        headers = info.context.get('headers', {})
+        authorization_header = headers.get('Authorization')
         if not authorization_header or not authorization_header.startswith('Bearer '):
             raise GraphQLError('Debes iniciar sesión')
 
         token = authorization_header.split()[1]
         try:
             access_token = AccessToken(token)
-            if not access_token:
-                raise GraphQLError('Token inválido')
         except TokenError:
             raise GraphQLError('Token inválido')
 
