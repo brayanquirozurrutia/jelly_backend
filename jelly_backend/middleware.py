4e5e187cc7493a5062d5601726d95047ecd4715d
BrayanQuirozUrrutia <brayanquirozurrutia@gmail.com>
2024-08-09 20:20:32 -0400

    :whale: FEAT: Modifica docker-compose y nginx

--- jelly_backend/middleware.py
+++ jelly_backend/middleware.py
@@ -8,5 +8,9 @@ class JWTAuthCookieMiddleware(MiddlewareMixin):
     """
     def process_request(self, request):
         access_token = request.COOKIES.get('access_token')
+        csrf_token = request.COOKIES.get('csrftoken')
+
         if access_token:
             request.META['HTTP_AUTHORIZATION'] = f'Bearer {access_token}'
+        if csrf_token:
+            request.META['HTTP_X_CSRFTOKEN'] = csrf_token
